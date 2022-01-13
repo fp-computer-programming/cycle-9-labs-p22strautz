e4a886dfd6ba6a1ec712f6e40f3598d9e3aacaf7
p22strautz <p22strautz@fairfieldprep.org>
2022-01-13 18:27:47 -0500

    lab 1-1, 2-1, 3-1

--- lab_3-1.py
+++ lab_3-1.py
@@ -1,27 +1,21 @@
 # Author: SCT (AMDG) 1/11/22
 
-sixth_letter = []
-not_foods = []
-short_foods = []
-
 def add_foods(lst):
-    for i, v in enumerate(lst):
+    sixth_letter = []
+    not_foods = []
+    short_foods = []
+    for x in lst:
         try:
-            if v[5] == i:
-                sixth_letter.append(v)
-            elif v != str:
-                not_foods.append(v)
-            else:
-                v[7] < i
-                short_foods.append(v)
-        except:
-            print("Invalid, input letters only")
-        finally:
-            print("sixth_letter:", sixth_letter)
-            print("not_foods:", not_foods)
-            print("short_foods:", short_foods)
+            sixth_letter.append(x[5])
+        except TypeError:
+            not_foods.append(x)
+        except IndexError:
+            short_foods.append(x)
 
+    print("sixth_letter: ", sixth_letter)
+    print("not_foods: ", not_foods)
+    print("short_foods: ", short_foods)
 
 add_foods(['potatoes', 'salsa', 'cherries', 'banana', 'apple'])
 add_foods(['naan', 'celery', 'buckwheat', 7, 'clementine'])
-add_foods(['cheeseburger', True, 'chicken', 'rice', 'radish'])
+add_foods(['cheesburger', True, 'chicken', 'rice', 'radish'])
